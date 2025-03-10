6e88e1877b196603bb05426051fbbdf353eaccf2
unknown <mustafasmart671st@gmail.com>
2025-03-11 00:00:37 +0300

    change

--- frontend/src/Pages/Auth/Register.tsx
+++ frontend/src/Pages/Auth/Register.tsx
@@ -40,7 +40,7 @@ function Register() {
             email: yup.string().email("Please enter a valid email").required("Email is required"),
             password: yup.string().min(8, "Password must be at least 8 characters").required("Password is required"),
             confirmPassword: yup.string().oneOf([yup.ref("password")], "Passwords must match").required("Confirm Password is required"),
-            agreeToTerms: yup.boolean().oneOf([true], "You must accept the terms & conditions"),
+            agreeToTerms: yup.boolean().oneOf([true], "You must accept the terms & conditions."),
         }),
         onSubmit(values) {
             const data: IRegisterBody = {
